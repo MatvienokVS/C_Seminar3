ea36dee68de227e6c2ac65358f44f573a56f86ba
MatvienokVS <Skavaava@gmail.com>
2022-12-25 20:12:39 +0300

    решение задачи 3

---
+++ Ex3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+using static System.Console;
+Clear();
+
+
+
+if (!int.TryParse(ReadLine(), out int num))
+{
+	WriteLine("Ошибка ввода. Введено не число.");
+	return;
+}
+else
+{
+	WriteLine("\\num/-\\num^3/");
+
+	while (num > 0)
+	{
+		WriteLine($"{"  "}{num} {" | "} {Math.Pow(num, 3)}");
+		WriteLine("-------------");
+		num--;
+	}
+}
